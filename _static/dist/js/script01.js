f9acc1b23f1610f93fbb858cdb275f36f378979e
take-1019 <tkchtmy@gmail.com>
2022-05-23 13:47:42 +0900

    フッター完成

--- _static/dist/js/script01.js
+++ _static/dist/js/script01.js
@@ -7,3 +7,25 @@ let mySwiper = new Swiper(".swiper", {
   },
   speed: 2000,
 });
+
+// スクロールするとロゴの色変更
+$(function () {
+  $(window).on("scroll", function () {
+    const sliderHeight = $(".mv").height();
+    if (sliderHeight - 30 < $(this).scrollTop()) {
+      $(".js-header").addClass("headerColorScroll");
+    } else {
+      $(".js-header").removeClass("headerColorScroll");
+    }
+  });
+});
+
+$(function() {
+(window).on('scroll', function () {
+　if (400 < jQuery(this).scrollTop()) {
+　　jQuery('.js-header').addClass('headerColorScroll');
+　} else {
+　　jQuery('.js-header').removeClass('headerColorScroll');
+　}
+});
+});
