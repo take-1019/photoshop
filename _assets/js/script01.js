ac7f8ea3020a1f555d7ce2985853da1cd38624cd
take-1019 <tkchtmy@gmail.com>
2022-05-23 15:08:30 +0900

    ヘッダーアニメーション

--- _assets/js/script01.js
+++ _assets/js/script01.js
@@ -22,10 +22,38 @@ $(function () {
 
 $(function() {
 (window).on('scroll', function () {
-　if (400 < jQuery(this).scrollTop()) {
-　　jQuery('.js-header').addClass('headerColorScroll');
-　} else {
-　　jQuery('.js-header').removeClass('headerColorScroll');
-　}
+  if (400 < jQuery(this).scrollTop()) {
+  jQuery('.js-header').addClass('headerColorScroll');
+  } else {
+    jQuery('.js-header').removeClass('headerColorScroll');
+  }
 });
+});
+
+
+$(function () {
+  $(window).on("load scroll", function () {
+    var $header = $(".header");
+
+    // 200以上スクロールしたら処理
+    if ($(window).scrollTop() > 600) {
+      $header.addClass("sticky", "headerLogoScroll");
+    } else {
+      $header.removeClass("sticky", "headerLogoScroll");
+    }
+  });
+});
+
+// スクロールするとロゴ画像変更
+$(function () {
+  $(window).on("load scroll", function () {
+    var $header = $(".header");
+
+    // 200以上スクロールしたら処理
+    if ($(window).scrollTop() > 600) {
+      $(".js-header").addClass("headerLogoScroll");
+    } else {
+      $(".js-header").removeClass("headerLogoScroll");
+    }
+  });
 });
